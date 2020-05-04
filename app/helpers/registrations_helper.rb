0c6649e04cac3b99493bb0b5179d7ef1ff58290b
kyle-law <junwei_1121@hotmail.com>
2020-05-04 08:49:31 +0800

    Add method for registration toggle button

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,10 @@
 module RegistrationsHelper
-end
+    def registration_button(event, registration)
+      if registration
+        button_to 'Unregister', event_registration_path(event, registration), method: :delete
+      else
+        button_to 'Register Now', event_registrations_path(event)
+      end
+    end
+  end
+  
