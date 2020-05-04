16e285c185339751d8e991aedd4138992a4e95c6
kyle-law <junwei_1121@hotmail.com>
2020-05-04 08:41:00 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  root "events#index"
+  resources :users
+  root 'events#index'
+
+  get 'signup' => 'users#new'
 
   resources :events do
-    resources :registrations
+    resources :registrations, only: %i[new create destroy]
   end
 
-  resources :users
+  get 'events/filter/:filter' => 'events#index', as: :filtered_events
+
+  resource :session, only: %i[create destroy]
+  get 'signin' => 'sessions#new'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
