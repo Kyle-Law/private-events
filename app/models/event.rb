378f9ab168bf1d1dd74359b4ee20e74010496210
kyle-law <junwei_1121@hotmail.com>
2020-05-04 08:44:06 +0800

    Adjust models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
-  belongs_to :user
-  has_many :registrations
+  has_many :registrations, dependent: :destroy
+  belongs_to :creator, class_name: 'User'
+
+  scope :past, -> { where('date < ?', Time.now).order('date desc') }
+  scope :upcoming, -> { where('date > ?', Time.now).order('date asc') }
+  validates :title, :date, presence: true
+  validates :description, length: { minimum: 10 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
-  belongs_to :event
-  has_many :registrations
+  has_many :registrations, dependent: :destroy
+  has_many :events, foreign_key: :creator_id
+  validates :username, presence: true
+
+  def upcoming_events
+    registrations.select { |r| r.event.date > Time.now }
+  end
+
+  def previous_events
+    registrations.select { |r| r.event.date < Time.now }
+  end
 end
