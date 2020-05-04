5039617f45d9d58cc349f7a9f03b6ac2cca8857b
kyle-law <junwei_1121@hotmail.com>
2020-05-04 08:39:39 +0800

    Add events actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,48 @@
 class EventsController < ApplicationController
     def index
-        @events = Event.all
+      case params[:filter]
+      when 'past'
+        @events = Event.past
+        @title = 'Past Events'
+      else
+        @events = Event.upcoming
+        @title = 'Upcoming Events'
+      end
     end
-end
+  
+    def show
+      @event = Event.find(params[:id])
+      @registrations = @event.registrations
+      @registration = current_user.registrations.find_by(event_id: @event.id) if current_user
+    end
+  
+    def new
+      if current_user
+        @event = Event.new
+      else
+        redirect_to signin_path
+      end
+    end
+  
+    def create
+      @event = Event.new(event_params)
+      @event.creator = current_user
+      if @event.save
+        @event.registrations.create!(user: current_user)
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def event_params
+      params.require(:event).permit(:title, :description, :date)
+    end
+  
+    def set_event
+      @event = Event.find(params[:id])
+    end
+  end
+  
