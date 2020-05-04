4877b174ba6e4b0afc684a9da3a47e0eee3659e9
kyle-law <junwei_1121@hotmail.com>
2020-05-04 08:48:32 +0800

    Update actions for registrations and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
-end
+    private
+  
+    def require_signin
+      redirect_to signin_path, alert: 'Please sign in first!' unless current_user
+    end
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  
+    helper_method :current_user
+  end
+  
